7021d151532432deae1570113cf582062a56b284
VarelaCristianFacundo <cvarelagarcia@gmail.com>
2024-09-03 00:19:45 -0300

    weather add and remove functionality

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,15 +1,75 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createRoot } from 'react-dom/client';
-import './popup.css'
+
+import { Box, InputBase, IconButton, Paper, Grid2 } from '@mui/material'
+import { Add as AddIcon } from '@mui/icons-material'
+
+import '@fontsource/roboto'
+import './popup.css';
+import WeatherCard from './WeatherCard/WeatherCard';
 
 // My React component
-const Test = () => <img src="icon.png" />
+const App = () => {
+    const [cities, setCities] = useState<string[]>([
+        'Toronto',
+        'New York',
+        'Error',
+        'London',
+    ])
+    const [cityInput, setCityInput] = useState<string>('')
+
+    const handleCityButtonClick = () => {
+        if (cityInput) {
+            setCities([...cities, cityInput]);
+        }
+        setCityInput('');
+    }
+
+    const handleCityDeleteButton = (index: number) => {
+        cities.splice(index, 1)
+        setCities([...cities])
+    }
+
+
+    return (
+        <Box mx="8px" my="16px">
+            <Grid2 container>
+                <Grid2>
+                    <Paper>
+                        <Box px="15px" py="5px">
+                            <InputBase
+                                placeholder="Add a city"
+                                inputProps={{
+                                    'aria-label': 'add a city',
+                                }}
+                                value={cityInput}
+                                onChange={(e) => setCityInput(e.target.value)}
+                            />
+                            <IconButton onClick={handleCityButtonClick}>
+                                <AddIcon />
+                            </IconButton>
+                        </Box>
+                    </Paper>
+                </Grid2>
+            </Grid2>
+
+            {
+                cities.map((city, index) => (
+                    <WeatherCard
+                        key={index}
+                        city={city}
+                        onDelete={() => handleCityDeleteButton(index)} />
+                ))
+            }
+            <Box height="16px" />
+        </Box>
+    );
+}
 
 // A root container
 const rootElement = document.createElement('div');
 document.body.appendChild(rootElement);
 
-// A root and render my component
+// Create a root and render the App component
 const root = createRoot(rootElement);
-root.render(<Test />);
-
+root.render(<App />);
---
+++ src/utils/api.ts
@@ -0,0 +1,37 @@
+const OPEN_WEATHER_API_KEY = 'e1aeeb160e946cc9bc8e660be59017b9'
+
+export interface OpenWeatherData {
+  name: string
+  main: {
+    feels_like: number
+    humidity: number
+    pressure: number
+    temp: number
+    temp_max: number
+    temp_min: number
+  }
+  weather: {
+    description: string
+    icon: string
+    id: number
+    main: string
+  }[]
+  wind: {
+    deg: number
+    speed: number
+  }
+}
+
+export async function fetchOpenWeatherData(
+  city: string
+): Promise<OpenWeatherData> {
+  const res = await fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${OPEN_WEATHER_API_KEY}`
+  )
+  if (!res.ok) {
+    throw new Error(`HTTP error! status: ${res.status}`)
+  }
+
+  const data: OpenWeatherData = await res.json()
+  return data
+}
