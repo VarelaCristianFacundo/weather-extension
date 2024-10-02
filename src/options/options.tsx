549a372cefab234c47c7e782467a813641aca42b
Varela Cristian Facundo <cvarelagarcia@gmail.com>
2024-10-01 21:55:48 -0300

    feat handlechange and handlesave

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,29 +1,84 @@
 import '@fontsource/roboto';
 import {
     Box,
+    Button,
     Card,
     CardContent,
     Grid2,
     TextField,
     Typography
 } from '@mui/material';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createRoot } from 'react-dom/client';
+import { LocalStorageOptions, getStoredOptions, setStoredOptions } from '../utils/storage';
 import './options.css';
+
+
+type FormState = 'ready' | 'saving'
 
 // My React component
 const Test = () => {
+    const [options, setOptions] = useState<LocalStorageOptions | null>(null)
+    const [formState, setFormState] = useState<FormState>('ready')
+
+    useEffect(() => {
+        getStoredOptions().then(options => setOptions(options))
+    }, [])
+
+    const handleHomeCityChange = (homeCity: string) => {
+        if (options) {
+            setOptions({
+                ...options,
+                homeCity
+            });
+        }
+    }
+
+    const handleSaveButtonClick = () => {
+        if (options) {
+            setFormState('saving');
+            setStoredOptions(options).then(() => {
+                setTimeout(() => {
+                    setFormState('ready');
+                    console.log('Options saved successfully');
+                }, 1000);
+            });
+        }
+    }
+
+    if (!options) {
+        return null;
+    }
+
+    const isFieldsDisabled = formState === 'saving'
+
     return (
         <Box mx="10%" my="2%">
             <Card>
                 <CardContent>
-                    <Grid2 container direction="column">
+                    <Grid2 container direction="column" spacing={4}>
                         <Grid2>
                             <Typography variant="h4">Weather Extension Options</Typography>
                         </Grid2>
                         <Grid2>
                             <Typography variant="body1">Home city name</Typography>
-                            <TextField fullWidth placeholder='enter a home city name' />
+                            <TextField
+                                fullWidth
+                                placeholder='enter a home city name'
+                                value={options.homeCity}
+                                onChange={event => handleHomeCityChange(event.target.value)}
+                                disabled={isFieldsDisabled}
+                            />
+                        </Grid2>
+                        <Grid2>
+                            <Button
+                                variant='contained'
+                                color='primary'
+                                onClick={handleSaveButtonClick}
+                                disabled={isFieldsDisabled}
+                            >
+                                {formState === 'ready' ? 'Save' : 'Saving...'}
+                            </Button>
                         </Grid2>
                     </Grid2>
                 </CardContent>
